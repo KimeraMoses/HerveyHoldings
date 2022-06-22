bd764244a204e738983e31f2ae2d95f58897ab9c
KimeraMoses <kimeramoses001@gmail.com>
2022-06-22 23:45:18 +0300

    Fix: partners page lit review

--- src/components/Pages/Partners/OurPartners.js
+++ src/components/Pages/Partners/OurPartners.js
@@ -12,7 +12,7 @@ const OurPartners = () => {
       <Breadcrumb title="Our Partners" />
       <div className={classes.partners__details}>
         <p>
-          Hervey Holdings Ltd will be celebrated its 6th birthday in October
+          Hervey Holdings Ltd celebrated its 6th birthday in October
           2021, since transforming from our earlier trading name. We have since
           worked alongside the worlds major engineering companies such as the
           following;
